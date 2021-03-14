357d782d362842842c92f506ef40a4c2768ecb99
Billy <billypurnama007@gmail.com>
2021-03-14 17:44:31 +0700

    Add sidebar data for navbar

---
+++ src/components/constant/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as AiIcons from 'react-icons/ai'
+
+export const SideBar = [
+	{
+		title: 'Pokémon List',
+		path: '/',
+		icon: <AiIcons.AiFillHome />,
+	},
+	{
+		title: 'My Pokémon List',
+		path: '/my-list',
+		icon: <AiIcons.AiFillHeart />,
+	},
+]
